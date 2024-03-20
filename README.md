542e6d609f52d50deab2c6f9dcca5d3b49a861a8
Nita Youssef <nitayoussef3@gmail.com>
2024-03-20 14:18:54 -0700

    nn

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# AirBnB_clone_v2
-AirBnB_clone_v2
+ #0x02. AirBnB clone - MySQL
--- models/state.py
+++ models/state.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+""" holds class State"""
 import models
-from models.city import City
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    if models.is_type == "db":
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
     else:
         name = ""
 
-    if models.is_type != 'db':
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            cities_list = []
-            all_cities = models.storage.all(City).values()
-            for city in all_cities:
+            """fs getter attribute that returns City instances"""
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
                 if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+                    list_city.append(city)
+            return list_city
