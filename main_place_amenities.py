36d77c44c4719cfc28147d9fac213563c3e908c0
useAllas <nitayoussef3@gmail.com>
2024-04-19 10:58:02 -0700

    ds

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -2,6 +2,11 @@
 """ Test link Many-To-Many Place <> Amenity
 """
 from models import *
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
 
 # creation of a State
 state = State(name="California")
