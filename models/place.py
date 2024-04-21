b1a5637bcbdbdec7f276ae2ac7ace599ae1ddc45
useAllas <nitayoussef3@gmail.com>
2024-04-21 08:30:46 -0700

    nn

--- models/place.py
+++ models/place.py
@@ -12,7 +12,7 @@ if models.storage_t == 'db':
                                  ForeignKey('places.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
-                          Column('amenity_id', VARCHAR(60, collation='utf8mb4_0900_ai_ci'),
+                          Column('amenity_id', String(60),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True))
@@ -22,7 +22,7 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        city_id = Column(VARCHAR(60, collation='latin1_swedish_ci'), ForeignKey('cities.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+        city_id = Column(String(60, collation='latin1_swedish_ci'), ForeignKey('cities.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
